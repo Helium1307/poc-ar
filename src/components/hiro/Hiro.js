740f600733c5bd7a5bd07b6be739c639dda56574
Helium1307 <heliofilho1307@hotmail.com>
2022-04-01 19:34:52 -0300

    added ar

---
+++ src/components/hiro/Hiro.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { AFrameRenderer, Marker } from "react-web-ar";
+
+function Hiro() {
+  return (
+    <AFrameRenderer
+      arToolKit={{ sourceType: "image", sourceUrl: "./images/hiro_marker.png" }}
+      stats
+    >
+      <Marker parameters={{ preset: "hiro" }}>
+        <a-box
+          color="pink"
+          material="opacity: 1;"
+          position="0 0.003 0"
+          scale="0.4 0.4 0.4"
+        >
+          <a-animation
+            attribute="rotation"
+            to="360 0 0"
+            dur="5000"
+            easing="linear"
+            repeat="indefinite"
+          />
+        </a-box>
+      </Marker>
+    </AFrameRenderer>
+  );
+}
+
+export default Hiro;
