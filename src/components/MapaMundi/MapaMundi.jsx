755374400210dd922846d3ea83c11c4d55072ec0
Helium1307 <heliofilho1307@hotmail.com>
2022-04-01 23:24:35 -0300

    added mapaMundi component

---
+++ src/components/MapaMundi/MapaMundi.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AFrameRenderer, Marker } from "react-web-ar";
+import mapaMundiImage from "./mapa_mundi.jpg";
+
+function MapaMundi() {
+  return (
+    <AFrameRenderer
+      arToolKit={{
+        sourceType: "webcam",
+        sourceUrl: "./images/hiro_marker.png",
+      }}
+    >
+      <Marker parameters={{ preset: "hiro" }}>
+        <a-assets-item img id="mundiMap" src={mapaMundiImage} />
+
+        <a-sphere src="#mundiMap" radius="1" segments-height="53">
+          <a-animation
+            attribute="rotation"
+            dur="1000"
+            from="1 -90 90"
+            to="360 -90 90"
+            easing="linear"
+            repeat="indefinite"
+          />
+        </a-sphere>
+      </Marker>
+    </AFrameRenderer>
+  );
+}
+
+export default MapaMundi;
