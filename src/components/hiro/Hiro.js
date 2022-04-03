9cead033186ed7ac109100334d7cc5f163e726b5
Helium1307 <heliofilho1307@hotmail.com>
2022-04-03 15:24:32 -0300

    added .obj asset

--- src/components/hiro/Hiro.js
+++ src/components/hiro/Hiro.js
@@ -11,9 +11,15 @@ function Hiro() {
     >
       <a-assets-item
         id="modelo"
-        src="../../assets/modelo3d.gltf"
+        src="https://cdn.glitch.com/d035a109-3c17-4c09-a808-395f69b2e93b%2Fmodelo3d.gltf?v=1587326228301"
       ></a-assets-item>
       <Marker parameters={{ preset: "hiro" }}>
+        <a-entity
+          id="modelo3D"
+          position="0 0 0"
+          scale="0.05 0.05 0.05"
+          gltf-model="#modelo"
+        ></a-entity>
         <a-entity geometry="primitive: box" material="color: red"></a-entity>
       </Marker>
     </AFrameRenderer>
