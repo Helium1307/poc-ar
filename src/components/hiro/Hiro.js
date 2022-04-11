b3364fab125fff75419da784bfce7fffdb09ede6
Helium1307 <heliofilho1307@hotmail.com>
2022-04-11 20:22:32 +0100

    adjustment

--- src/components/hiro/Hiro.js
+++ src/components/hiro/Hiro.js
@@ -19,6 +19,7 @@ function Hiro() {
         <a-entity
           id="modelo3D"
           position="0 0 0"
+          rotation="-90 0 0"
           scale="0.05 0.05 0.05"
           gltf-model="url(https://cdn.glitch.com/d035a109-3c17-4c09-a808-395f69b2e93b%2Fmodelo3d.gltf?v=1587326228301)"
         ></a-entity>
