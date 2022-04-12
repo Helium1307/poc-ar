53d1b53888543937c9a8756564f2202bee6884cc
Helium1307 <heliofilho1307@hotmail.com>
2022-04-12 18:02:43 +0100

    added new way to import 3d model

--- src/components/hiro/Hiro.js
+++ src/components/hiro/Hiro.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { AFrameRenderer, Marker } from "react-web-ar";
+import Modelo from "../../assets/modelo.gltf";
+import CouchObj from "../../assets/couch.obj";
+import CouchMtl from "../../assets/couch.mtl";
+
+//"url(https://raw.githack.com/Helium1307/poc-ar/master/src/assets/modelo.gltf)"
 // https://cdn.glitch.com/d035a109-3c17-4c09-a808-395f69b2e93b%2Fmodelo3d.gltf?v=1587326228301
 /*obj-model="obj: url(https://rishavjayswal.github.io/augmented-reality/resources/couch.obj); 
 mtl: url(https://rishavjayswal.github.io/augmented-reality/resources/couch.mtl)" */
@@ -23,7 +28,7 @@ function Hiro() {
           position="0 0 0"
           rotation="-90 0 0"
           scale="0.05 0.05 0.05"
-          gltf-model="url(https://raw.githack.com/Helium1307/poc-ar/master/src/assets/modelo.gltf)"
+          gltf-model={Modelo}
         ></a-entity>
         {/* <a-entity geometry="primitive: box" material="color: red"></a-entity>
         <a-entity
